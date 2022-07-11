c54b54a51d0303515edb2ede2c9ef37d6df26879
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-07-11 14:46:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# springboot-kafka-real-work-project
+ Learn to Use Apache Kafka to Exchange Messages Between Producer and Consumer in Spring Boot Applications & Microservices
---
+++ src/main/java/com/ratz/springbootkafkarealworkproject/SpringbootKafkaRealWorkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ratz.springbootkafkarealworkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRealWorkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaRealWorkProjectApplication.class, args);
+	}
+
+}
